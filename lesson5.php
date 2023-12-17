a3283ddd17a8331c560803c9304009b46ac0c136
Igor <idonchenko25@gmail.com>
2023-12-18 00:50:28 +0200

    "2"

--- lesson5.php
+++ lesson5.php
@@ -6,7 +6,7 @@
 // $n1 = 1;
 // $n2 = 0;
 // for($i = 1; $i<=$num; $i++){
-//     echo $n. ' ';
+//     echo $n. ' , ';
 //     $n= $n1+$n2;
 //     $n1 = $n2;
 //     $n2 = $n;
@@ -15,21 +15,21 @@
 //}
 //fibonacci(10);
 
-    function fibonacciSum($n){
-          $a = 0;
-          $b = 1;
-          $sum = 0;
+     function fibonacciSum($n){
+           $a = 0;
+           $b = 1;
+           $sum = 0;
 
-        for($i= 0; $i<$n; $i++){
-          $sum += $a;
-          $r = $a + $b;
-          $a = $b;
-          $b = $r;
-        }
-        return $sum;
-    }
-        echo fibonacciSum(10);
-    ?>
+         for($i= 0; $i<$n; $i++){
+           $sum += $a;
+           $r = $a + $b;
+           $a = $b;
+           $b = $r;
+         }
+         return $sum;
+     }
+         echo fibonacciSum(10);
+     ?>
 
 
 
