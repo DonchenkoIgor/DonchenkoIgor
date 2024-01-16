f5f3ac97859d237bcc50376b75105e900ea87222
Igor <idonchenko25@gmail.com>
2024-01-16 17:24:57 +0200

    lesson 8

--- index.php
+++ index.php
@@ -1,5 +1,40 @@
+<?php
+session_start();
+?>
+<?php
+if(!isset($_COOKIE['counter'])){
+    $counter = 0;
+}else {
+    $counter = $_COOKIE['counter'];
+}
+$counter++;
+
+setcookie('counter', $counter);
+setcookie('test' , 1);
+$_COOKIE['test'];
+
+
+if(!empty($_POST['email'])) {
+    if (empty($_SESSION['user'])) {
+        $_SESSION['user'] = [];
+    }
+
+
+    $_SESSION['is_logged_in'] = true;
+
+    $id = count($_SESSION['user']);
+    $_SESSION['last_email'] = $id;
+
+    $_SESSION['user'] = [
+        'email' => $_POST['email'],
+        'password' => $_POST['password'],
+    ];
+    $_SESSION['form_submited'] = true;
+}
+?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -11,42 +46,49 @@
     <title>Form</title>
 </head>
 <body>
-      <div class="container mt-4">
-          <?php
-          var_dump($_POST);
-          ?>
-          <br><br>
-          <?php if(!empty($_POST)) : ?>
-              <div class="alert alert-success" role="alert">
-              <?php
-                   $email = $_POST['email'];
-                    if(strlen(trim($email)) <= 3)
-                       echo 'Такого email не існує';
-                       else{
-              echo 'Ви намагались залогінитись з'. ' '. $_POST['email'];
-              }  ?>
-          </div>
-          <?php endif; ?>
-          <form  method="post">
-              <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">Email</label>
-                  <input
-                      type="email"
-                      class="form-control"
-                      name="email"
-                  >
-              </div>
-              <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input
-                      type="password"
-                      class="form-control"
-                      name="password"
-                  >
-              </div>
-              <button type="submit" class="btn btn-primary">Sign up</button>
-          </form>
-      </div>
+<div class="container mt-4">
+    <br><br>
+    <?php if(!empty($_POST['email'])) : ?>
+        <div class="alert alert-success" role="alert">
+            <?php
+            $email = $_POST['email'];
+            if(strlen(trim($email)) <= 3)
+                echo 'Такого email не існує';
+            else{
+                echo 'Ви увійшли з'. ' '. $_POST['email'];
+            }
+            ?>
+
+            <br>
+        </div>
+    <?php endif; ?>
+    <?php
+    var_dump($_COOKIE);
+    var_dump($_SESSION);
+    ?>
+    <?php
+    if (empty($_SESSION['is_logged_in']) || !$_SESSION['is_logged_in']) :
+    ?>
+    <form  method="post">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email</label>
+            <input
+                    type="email"
+                    class="form-control"
+                    name="email"
+            >
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input
+                    type="password"
+                    class="form-control"
+                    name="password"
+            >
+        </div>
+        <button type="submit" class="btn btn-primary">Sign up</button>
+    </form>
+    <?php endif; ?>
+</div>
 </body>
 </html>
-
